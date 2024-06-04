7bf80111696f19d6c89aa7f080e54d8fdc5674cf
janhvif <janhvifulzele1@gmail.com>
2024-06-04 14:15:00 +0530

    Add files via upload

---
+++ sql task1.sql
@@ -0,0 +1,90 @@
+insert into shops values(7,'naeem bhai',700,'retaail',30,'naeem shops','ramdaspeth',8890,700000,80000,true)
+select * from shops,
+insert into shops values(8,'dhirubhai',800,'wholesale',40,'ramani shops','bhilgaon',5678,800000,90000,true)
+select * from shops,
+insert into shops values(9,'rita',900,'retail',60,'rita kirana','besa road',9967,400000,20000,true)
+select * from shops,
+insert into shops values(10,'neha',300,'wholesale',40,'dhupia farms','lakadganj',8899,350000,40000,true)
+select * from shops,
+insert into shops values(11,'vikas',780,'retail',55,'duhan kirana','prajapati nagar',9994,200000,20000,true)
+select * from shops,
+insert into shops values(12,'sajal',240,'wholesale',37,'gupta kirana','viman nagar',9325,100000,30000,true)
+select * from shops,
+insert into shops values(13,'prajwal',120,'retail',23,'shende kirana','lonavla',8945,400000,10000,true)
+select * from shops	,
+insert into shops values(14,'hina',351,'retail',37,'hina shops','bahadura',9998,230000,20000,true)
+select * from shops,
+insert into shops values(15,'heema',245,'wholesale',67,'heema dukan','ravanwadi',8856,120000,30000,true)
+select * from shops,
+insert into shops values(16,'hiral',78,'online',36,'hiral shops','lahore',7899,320000,50000,true)
+select * from shops,
+insert into shops values(17,'pannalal',23,'retail',78,'panna dukan','karol bag',9089,124000,22000,true)
+select * from shops,
+insert into shops values(18,'hiralal',42,'wholesale',33,'javeri duka','rajori garden',6788,230000,32000,true)
+select * from shops,
+insert into shops values(19,'jiyan',67,'online',56,'kirana vale','hiroshima',9888,430000,23444,true)
+select * from shops,
+insert into shops values(20,'shinchan',88,'wholesale',33,'shopsy','nagasaki',3488,350000,20000,true)
+select * from shops,
+insert into shops values(21,'riyal',43,'wholesale',13,'menka dukan','itwari',9995,200000,34000,true)
+select * from shops,
+insert into shops values(22,'priyal',34,'wholesale',27,'shende kirna','gandhibag',9958,400000,40000,true)
+select * from shops,
+insert into shops values(23,'sheena bora',67,'retail',64,'bora shops','viman nagar',9347,300000,20000,true)
+select * from shops,
+insert into shops values(24,'shruti',78,'retail',23,'pantawne shops','samta nagar',9866,450000,30000,true)
+select * from shops,
+insert into shops values(25,'nimisha',56,'online',80,'tirpude shops','kamthi nagar',8889,200000,10000,true)
+select * from shops,
+insert into shops values(26,'rambhai',89,'wholesale',70,'hitachi shops','ravi nagar',8923,340000,23000,true)
+select * from shops,
+insert into shops values(27,'nitaji',45,'wholesale',59,'kiran shops','inam nagar',9766,270000,15000,true)
+select * from shops,
+insert into shops values(28,'pawan',78,'retail',35,'pawan dukan','somalwada',8669,350000,30000,true)
+select * from shops,
+insert into shops values(29,'hitesh',23,'wholesale',42,'jai maa dukan','nari road',7890,260000,40000,true)
+select * from shops,
+insert into shops values(30,'ritesh',77,'retail',88,'miran shops','automotive square',8870,400000,30000,true)
+select * from shops,
+insert into shops values(31,'nayan',51,'retail',32,'koche shops','laxmi square',6790,120000,21000,true)
+select * from shops,
+insert into shops values(32,'roshni',56,'online',60,'wadhwani shops','teka naka square',9990,430000,10000,true)
+select * from shops,
+insert into shops values(33,'chirag',88,'retail',70,'hemani shops','ram naka square',7789,230000,20000,true)
+select * from shops,
+insert into shops values(34,'drakshi',34,'wholesale',63,'tagde shops','hislop square',8888,330900,30000,true)
+select * from shops,
+insert into shops values(35,'peru',31,'wholesale',89,'rama shops','futala square',7789,200000,10000,true)
+select * from shops,
+insert into shops values(36,'siddhant',58,'wholesale',39,'ghode shops','wathoda square',4500,400000,20000,true)
+select * from shops,
+insert into shops values(38,'ojus',45,'online',88,'ojus kirana','bhande square',9087,200000,40000,true)
+select * from shops
+insert into shops values(39,'janbandhu',84,'online',28,'janbandhu shops','hade square',9999,200000,21000,true)
+select * from shops
+insert into shops values(40,'rahate',23,'wholesale',26,'rahate shops','jilha square',9997,270000,30000,true)
+select * from shops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
